c908538feb3a4bdbebd149c9b2f46cf4760da357
Matias <matias_arenhard@hotmail.com>
2022-03-14 22:10:49 -0300

    create check box and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  return (
-    <div>Hey
-    </div>
-  );
+  const [theme, setTheme] = useState("light");
+
+  const handleThemeChange = ({ target }) => setTheme(target.checked ? "dark" : "light");
+
+  return <main className={theme}>
+    Texto
+    <label>
+      <input type="checkbox" checked={theme === "dark"} onChange={handleThemeChange}/>Modo escuro
+    </label>
+  </main>;
 }
 
 export default App;
