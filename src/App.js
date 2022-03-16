f97496dd5713c639894f63fd057ac84ee9ccc275
Matias <matias_arenhard@hotmail.com>
2022-03-15 21:43:42 -0300

    add localstorage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import Box from "../src/components/box";
 import ThemeContext from "../src/contexts/ThemeContext";
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => { 
+    const storage_theme = localStorage.getItem("theme");
+    return storage_theme || "light";
+  });
 
-  const handleThemeChange = ({ target }) => setTheme(target.checked ? "dark" : "light");
+  const handleThemeChange = ({ target }) => {
+    const newTheme = target.checked ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  };
+  
 
   return(
     <ThemeContext.Provider value={theme}>
